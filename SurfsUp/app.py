f7a611bfd7c86ef5a4fd5664e72769aab54f6a35
mmyrahim <mmyrahim@outlook.com>
2023-09-20 23:01:19 +0100

    adding the app screenshots code

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -29,4 +29,72 @@ def home():
     return (
         "Welcome to the Climate Analysis API!<br/>"
         "Available Routes:<br/>"
-        "/api/v1.0/precipitation - Precipitation data 
+        "/api/v1.0/precipitation - Precipitation data for the last year<br/>"
+        "/api/v1.0/stations - List of weather stations<br/>"
+        "/api/v1.0/tobs - Temperature observations for the most active station (last year)<br/>"
+    )
+
+# precipitation route
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # create a new session
+    session = create_session()
+    
+    # calculate the date one year from the last date in the dataset
+    most_recent_date = session.query(func.max(Measurement.date)).scalar()
+    most_recent_date = dt.datetime.strptime(most_recent_date, '%Y-%m-%d')
+    one_year_prior = most_recent_date - dt.timedelta(days=365)
+    
+    # query and format precipitation data
+    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= one_year_prior).all()
+    
+    # close the session
+    session.close()
+    
+    # convert the results to a dictionary
+    precipitation_data = {date: prcp for date, prcp in results}
+    
+    return jsonify(precipitation_data)
+
+# stations route
+@app.route("/api/v1.0/stations")
+def stations():
+    # create a new session
+    session = create_session()
+    
+    # query and format station data
+    results = session.query(Station.station).all()
+    
+    # close the session
+    session.close()
+    
+    # convert the results to a list
+    station_data = [station for station, in results]
+    
+    return jsonify(station_data)
+
+# TOBS route
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # create a new session
+    session = create_session()
+    
+    # calculate the date one year from the last date in the dataset
+    most_recent_date = session.query(func.max(Measurement.date)).scalar()
+    most_recent_date = dt.datetime.strptime(most_recent_date, '%Y-%m-%d')
+    one_year_prior = most_recent_date - dt.timedelta(days=365)
+    
+    # query and format temperature observations
+    results = session.query(Measurement.tobs).filter(Measurement.station == "USC00519281").filter(Measurement.date >= one_year_prior).all()
+    
+    # close the session
+    session.close()
+    
+    # convert the results to a list
+    tobs_data = [tobs for tobs, in results]
+    
+    return jsonify(tobs_data)
+
+# run the app
+if __name__ == "__main__":
+    app.run(debug=True)
